a7305ffeb5010f9a7ee59a22d6f399a896191f01
李大雷 <1420551506@qq.com>
2020-09-24 17:27:59 +0800

    Output

--- 3118005335/src/main/java/com/company/Output.java
+++ 3118005335/src/main/java/com/company/Output.java
@@ -8,8 +8,7 @@ public class Output {
     public static void output(String outputPath, String result) {
         File f = new File(outputPath);
         try (FileWriter fr = new FileWriter(f)) {
-            String data = result;
-            char[] cs = data.toCharArray();
+            char[] cs = result.toCharArray();
             fr.write(cs);
             fr.close();
             System.out.println("结果已写入" + outputPath);
